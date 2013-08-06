82bca5cd6a911293247e623f1ed473acb80ef803
istr <github@ingostruck.de>
2013-08-06 12:28:14 +0200

    separate dialyzer flags for R16B

--- dialyzer_flags.sh
+++ dialyzer_flags.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 OTP=`erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell | sed -e 's/"//g'`
 
-FLAGS="-n -nn -Wunmatched_returns -Werror_handling -Wunderspecs"
+FLAGS="-Wunmatched_returns -Werror_handling -Wunderspecs"
 		
 case $OTP in
-R16*|R15B03) FLAGS="$FLAGS -Wrace_conditions --statistics" ;;
+R16*) FLAGS="$FLAGS -Wrace_conditions --statistics" ;;
+R15B03) FLAGS="$FLAGS -Wrace_conditions --statistics" ;;
 R15B02) FLAGS="$FLAGS --statistics" ;;
 esac
 
