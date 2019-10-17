b35ca87b1ed493a0f87358abc48c800077f77911
Kostis Sagonas <kostis@it.uu.se>
2019-10-17 11:59:32 +0200

    Enable a compiler option to shut off a warning

--- test/auto_export_test2.erl
+++ test/auto_export_test2.erl
@@ -1,7 +1,7 @@
 %%% -*- coding: utf-8 -*-
 %%% -*- erlang-indent-level: 2 -*-
 %%% -------------------------------------------------------------------
-%%% Copyright 2010-2011 Manolis Papadakis <manopapad@gmail.com>,
+%%% Copyright 2010-2019 Manolis Papadakis <manopapad@gmail.com>,
 %%%                     Eirini Arvaniti <eirinibob@gmail.com>
 %%%                 and Kostis Sagonas <kostis@cs.ntua.gr>
 %%%
@@ -20,7 +20,7 @@
 %%% You should have received a copy of the GNU General Public License
 %%% along with PropEr.  If not, see <http://www.gnu.org/licenses/>.
 
-%%% @copyright 2010-2011 Manolis Papadakis, Eirini Arvaniti and Kostis Sagonas
+%%% @copyright 2010-2019 Manolis Papadakis, Eirini Arvaniti and Kostis Sagonas
 %%% @version {@version}
 %%% @author Manolis Papadakis
 %%% @doc This module tests whether auto-exporting is disabled when compiling
@@ -28,8 +28,11 @@
 
 -module(auto_export_test2).
 
+-compile(nowarn_unused_function).
+
 -define(PROPER_NO_TRANS, true).
 
 -include_lib("proper/include/proper.hrl").
 
-prop_1() -> ?FORALL(_, integer(), true).
+prop_1() ->
+  ?FORALL(_, integer(), true).
