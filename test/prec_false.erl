93d2c408f804c8ece3bf2edcb4578bb017418144
Kostis Sagonas <kostis@it.uu.se>
2020-04-01 14:14:47 +0200

    Add proper exports instead of export_all

--- test/prec_false.erl
+++ test/prec_false.erl
@@ -1,7 +1,7 @@
 %%% -*- coding: utf-8 -*-
 %%% -*- erlang-indent-level: 2 -*-
 %%% -------------------------------------------------------------------
-%%% Copyright 2010-2011 Manolis Papadakis <manopapad@gmail.com>,
+%%% Copyright 2010-2020 Manolis Papadakis <manopapad@gmail.com>,
 %%%                     Eirini Arvaniti <eirinibob@gmail.com>
 %%%                 and Kostis Sagonas <kostis@cs.ntua.gr>
 %%%
@@ -20,12 +20,13 @@
 %%% You should have received a copy of the GNU General Public License
 %%% along with PropEr.  If not, see <http://www.gnu.org/licenses/>.
 
-%%% @copyright 2010-2011 Manolis Papadakis, Eirini Arvaniti and Kostis Sagonas
+%%% @copyright 2010-2020 Manolis Papadakis, Eirini Arvaniti and Kostis Sagonas
 %%% @version {@version}
 %%% @author Eirini Arvaniti
 
 -module(prec_false).
--compile(export_all).
+-export([command/1, initial_state/0, next_state/3,
+	 precondition/2, postcondition/3, foo/0, bar/0]).
 
 -include_lib("proper/include/proper.hrl").
 
