d901e831f3e44478c37dfc77cc57e62905138856
Kostis Sagonas <kostis@it.uu.se>
2021-02-20 09:50:16 +0100

    Strengthen a spec

--- src/proper_target.erl
+++ src/proper_target.erl
@@ -1,7 +1,7 @@
 %%% -*- coding: utf-8 -*-
 %%% -*- erlang-indent-level: 2 -*-
 %%% -------------------------------------------------------------------
-%%% Copyright (c) 2017-2020 Andreas Löscher <andreas.loscher@it.uu.se>
+%%% Copyright (c) 2017-2021 Andreas Löscher <andreas.loscher@it.uu.se>
 %%%                     and Kostis Sagonas <kostis@it.uu.se>
 %%%
 %%% This file is part of PropEr.
@@ -19,7 +19,7 @@
 %%% You should have received a copy of the GNU General Public License
 %%% along with PropEr.  If not, see <http://www.gnu.org/licenses/>.
 
-%%% @copyright 2017-2020 Andreas Löscher and Kostis Sagonas
+%%% @copyright 2017-2021 Andreas Löscher and Kostis Sagonas
 %%% @version {@version}
 %%% @author Andreas Löscher
 
@@ -274,7 +274,7 @@ strategy(Strat) ->
 %% -----------------------------------------------------------------------------
 
 %% @private
--spec init(Args :: [{strategy(), strategy_data()}]) -> {ok, state()}.
+-spec init(Args :: [{strategy(), strategy_data()},...]) -> {ok, state()}.
 init([{Strategy, Data}]) ->
   {ok, #state{strategy = Strategy, data = Data}}.
 
