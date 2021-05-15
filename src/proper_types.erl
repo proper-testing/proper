a7dc5415928ca3227270eb1131e9a4bb3c8b1455
Kostis Sagonas <kostis@it.uu.se>
2021-05-15 23:46:13 +0200

    Eliminate an edoc warning

--- src/proper_types.erl
+++ src/proper_types.erl
@@ -1,8 +1,7 @@
-%%% -*- coding: utf-8 -*-
-%%% -*- erlang-indent-level: 2 -*-
+%%% -*- coding: utf-8; erlang-indent-level: 2 -*-
 %%% -------------------------------------------------------------------
-%%% Copyright 2010-2020 Manolis Papadakis <manopapad@gmail.com>,
-%%%                     Eirini Arvaniti <eirinibob@gmail.com>
+%%% Copyright 2010-2021 Manolis Papadakis <manopapad@gmail.com>,
+%%%                     Eirini Arvaniti <eirinibob@gmail.com>,
 %%%                 and Kostis Sagonas <kostis@cs.ntua.gr>
 %%%
 %%% This file is part of PropEr.
@@ -20,7 +19,7 @@
 %%% You should have received a copy of the GNU General Public License
 %%% along with PropEr.  If not, see <http://www.gnu.org/licenses/>.
 
-%%% @copyright 2010-2020 Manolis Papadakis, Eirini Arvaniti and Kostis Sagonas
+%%% @copyright 2010-2021 Manolis Papadakis, Eirini Arvaniti, and Kostis Sagonas
 %%% @version {@version}
 %%% @author Manolis Papadakis
 
@@ -232,8 +231,10 @@
 
 -opaque type() :: {'$type', [type_prop()]}.
 %% A type of the PropEr type system
-%% @type raw_type(). You can consider this as an equivalent of {@type type()}.
+
 -type raw_type() :: type() | [raw_type()] | loose_tuple(raw_type()) | term().
+%% You can consider this as an equivalent of {@type type()}.
+
 -type type_prop_name() :: 'kind' | 'generator' | 'reverse_gen' | 'parts_type'
 			| 'combine' | 'alt_gens' | 'shrink_to_parts'
 			| 'size_transform' | 'is_instance' | 'shrinkers'
